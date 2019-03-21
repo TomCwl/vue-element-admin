2bc55ea2c92a6460ab61fad2a409528344484627
Leon <cwljx@icloud.com>
2019-03-21 16:06:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -47,10 +47,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../../../../usr/loacl/nginx/html/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../../../usr/loacl/nginx/html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
 
     /**
